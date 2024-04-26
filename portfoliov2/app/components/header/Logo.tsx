a027b00502e5534e99e9ce2d39e6c627dcf30894
TakashiNishii <takashi@geeklys.com>
2024-04-26 18:55:00 -0400

    :wrench: Add link on logo

--- portfoliov2/app/components/header/Logo.tsx
+++ portfoliov2/app/components/header/Logo.tsx
@@ -1,10 +1,14 @@
 import { CommandLineIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <div className="flex flex-row gap-2 link-hover cursor-pointer text-white  items-center hover:text-accent">
+    <Link
+      href="/?section=home"
+      className="flex flex-row gap-2 link-hover cursor-pointer text-white  items-center hover:text-accent"
+    >
       <CommandLineIcon className="w-8 h-8" />
       <h2 className=" text-xl font-semibold">Takashi Nishi</h2>
-    </div>
+    </Link>
   );
 };
